d01b35b5e9eaf688164a4efcd45181788fb9fe29
Doug Gilliland <doug@douglasgilliland.com>
2021-07-10 20:07:51 -0400

    VGA and UART tested and working.

--- CPU_top.vhd
+++ CPU_top.vhd
@@ -49,6 +49,13 @@ ENTITY CPU_top IS
 		SMG_Data		: out std_logic_vector(7 downto 0);
 		Scan_Sig		: out std_logic_vector(2 downto 0);
 		
+		-- VGA
+		vga_r			: out std_logic_vector(4 downto 0);
+		vga_g			: out std_logic_vector(5 downto 0);
+		vga_b			: out std_logic_vector(4 downto 0);
+		vga_hs		: OUT std_logic;
+		vga_vs		: OUT std_logic;
+		
 		-- UART
 		uart_rx		: in std_logic := '1';
 		uart_tx		: OUT std_logic
@@ -68,22 +75,30 @@ ARCHITECTURE beh OF CPU_top IS
 	signal w_peripWr				: std_logic;
 	signal w_peripRd				: std_logic;
 	
-	-- Peripherals
-	signal w_serialLoopback		: std_logic;
-	
 	-- Pushbutton
 	signal w_keyBuff				: std_logic;
 
-	-- Test Register
---	signal w_PerTestReg			: std_logic_vector(7 downto 0);
---	signal w_peripTRWr			: std_logic;
---	signal w_peripTRRd			: std_logic;
-	
 	-- Seven Segment Display
 	signal w_SevenSegData		: std_logic_vector(11 downto 0);
 	
+   -- UART
+	signal serialEn     			: std_logic;							-- 16x Serial Clock
+	signal w_UARTDataOut			: std_logic_vector(7 downto 0);
+	signal w_UARTWr    				: std_logic;
+	signal W_UARTRd    				: std_logic;
+	
+	-- VGA
+	signal w_videoR		: std_logic_vector(1 downto 0);
+	signal w_videoG		: std_logic_vector(1 downto 0);
+	signal w_videoB		: std_logic_vector(1 downto 0);
+	signal w_VDUDataOut	: std_logic_vector(7 downto 0);
+	signal W_VDUWr    	: std_logic;
+	signal W_VDURd    	: std_logic;
+
+	
 BEGIN
 
+	-- -----------------------------------------------------------------------------------------------------------------
 	-- The CPU
 	CPU : ENTITY work.cpu_001
 	PORT map 
@@ -96,18 +111,18 @@ BEGIN
 		o_peripWr				=> w_peripWr,
 		o_peripRd				=> w_peripRd
 	);
+	
+	-- -----------------------------------------------------------------------------------------------------------------
+	-- Peripherals
 	-- Peripheral read data mux
-	w_peripDataToCPU <=	"0000000"&w_keyBuff when (w_peripAddr = x"00") else
+	w_peripDataToCPU <=	"0000000"&w_keyBuff						when (w_peripAddr = x"00") else
+								w_SevenSegData(7 downto 0)				when (w_peripAddr = x"02") else
+								"0000"&w_SevenSegData(11 downto 8)	when (w_peripAddr = x"03") else
+								w_UARTDataOut								when (w_peripAddr(7 downto 1) = "0000010") else
+								w_VDUDataOut								when (w_peripAddr(7 downto 1) = "0000011") else
 								x"00";
 	w_keyBuff	<= i_KEY0;
 
-	-- Slow clock - used for debugging CPU
-	slowClock : ENTITY work.SlowClock
-	PORT MAP  (
-		i_clock		=> i_clock,
-		o_slowClock	=> w_slowPulse
-	);
-	
 	-- CPU Write Latches
 	latchLED : process (i_clock)
 	begin
@@ -123,8 +138,8 @@ BEGIN
 	end process;
 	
 	-- USB-Serial for UART
-	w_serialLoopback	<= uart_rx;
-	uart_tx				<= w_serialLoopback;
+--	w_serialLoopback	<= uart_rx;
+--	uart_tx				<= w_serialLoopback;
 
 	-- Seven Segment, 3 digits
 	sevSeg : entity work.Loadable_7SD_3LED
@@ -134,6 +149,74 @@ BEGIN
 		i_displayed_number 	=> w_SevenSegData,	-- 3 digits
 		o_Anode_Activate 		=> Scan_Sig,			-- 3 Anode signals
 		o_LED_out 				=> SMG_Data				-- Cathode patterns of 7-segment display
-	);	
+	);
+	vga_r <= w_videoR(1)&w_videoR(1)&w_videoR(0)&w_videoR(0)&w_videoR(0);
+	vga_g <= w_videoG(1)&w_videoG(1)&w_videoG(0)&w_videoG(0)&w_videoG(0)&w_videoG(0);
+	vga_b <= w_videoB(1)&w_videoB(1)&w_videoB(0)&w_videoB(0)&w_videoB(0);
+	
+	-- Grant's VGA driver
+	W_VDUWr <= '1' when ((w_peripAddr(7 downto 1) = "0000011") and (w_peripWr = '1')) else '0';
+	W_VDURd <= '1' when ((w_peripAddr(7 downto 1) = "0000011") and (w_peripRd = '1')) else '0';
+	
+	vdu : entity work.SBCTextDisplayRGB
+		port map (
+			clk		=> i_clock,
+			n_reset	=> '1',
+			-- CPU interface
+			n_WR		=> not W_VDUWr,
+			n_rd		=> not W_VDURd,
+			regSel	=> w_peripAddr(0),
+			dataIn	=> w_peripDataFromCPU,
+			dataOut	=> w_VDUDataOut,
+			-- VGA video signals
+			hSync		=> vga_hs,
+			vSync		=> vga_vs,
+			videoR0	=> w_videoR(0),
+			videoR1	=> w_videoR(1),
+			videoG0	=> w_videoG(0),
+			videoG1	=> w_videoG(1),
+			videoB0	=> w_videoB(0),
+			videoB1	=> w_videoB(1)
+			-- PS/2 keyboard
+--			ps2Clk	=> io_ps2Clk,
+--			ps2Data	=> io_ps2Data
+		);
+	
+	-- ACIA UART serial interface
+	w_UARTWr <= '1' when ((w_peripAddr(7 downto 1) = "0000010") and (w_peripWr = '1')) else '0';
+	W_UARTRd <= '1' when ((w_peripAddr(7 downto 1) = "0000010") and (w_peripRd = '1')) else '0';
+
+	acia: entity work.bufferedUART
+		port map (
+			clk		=> i_clock,     
+			n_WR		=> not w_UARTWr,
+			n_rd		=> not W_UARTRd,
+			regSel	=> w_peripAddr(0),
+			dataIn	=> w_peripDataFromCPU,
+			dataOut	=> w_UARTDataOut,
+			rxClkEn	=> serialEn,
+			txClkEn	=> serialEn,
+			rxd		=> uart_rx,
+			txd		=> uart_tx
+		);
+		
+	-- ____________________________________________________________________________________
+	-- Baud Rate Generator
+	-- Legal BAUD_RATE values are 115200, 38400, 19200, 9600, 4800, 2400, 1200, 600, 300
+	BaudRateGen : entity work.BaudRate6850
+	GENERIC map (
+		BAUD_RATE	=>  115200
+	)
+	PORT map (
+		i_CLOCK_50	=> i_clock,
+		o_serialEn	=> serialEn
+	);
+
+	-- Slow clock - used for debugging CPU
+	slowClock : ENTITY work.SlowClock
+	PORT MAP  (
+		i_clock		=> i_clock,
+		o_slowClock	=> w_slowPulse
+	);
 	
 END beh;
