0dc38e3b630d5192f75ab05d5146e94285867553
Doug Gilliland <doug@douglasgilliland.com>
2021-07-17 09:58:15 -0400

    Clean up the Peripherals - rename folder

--- CPU_top.vhd
+++ CPU_top.vhd
@@ -27,13 +27,13 @@ ENTITY CPU_top IS
 		o_LED			: INOUT std_logic;	-- LED on FPGA card
 		
 		-- 3 Digits, 7 Segment Display on FPGA card
-		o_SMG_Data	: out std_logic_vector(7 downto 0);
-		o_Scan_Sig	: out std_logic_vector(2 downto 0);
+		o_SMG_Data	: out std_logic_vector(7 downto 0);		-- 7 segments plus decimal point
+		o_Scan_Sig	: out std_logic_vector(2 downto 0);		-- 3 digits
 		
 		-- VGA - Mapped from 2:2:2 to 5:6:5 on FPGA card
-		o_vga_r		: out std_logic_vector(4 downto 0);
-		o_vga_g		: out std_logic_vector(5 downto 0);
-		o_vga_b		: out std_logic_vector(4 downto 0);
+		o_vga_r		: out std_logic_vector(4 downto 0);		-- 5 bits of red on FPGA card
+		o_vga_g		: out std_logic_vector(5 downto 0);		-- 6 bits of green on FPGA card
+		o_vga_b		: out std_logic_vector(4 downto 0);		-- 5 bits of blue on FPGA card
 		o_vga_hs		: OUT std_logic;
 		o_vga_vs		: OUT std_logic;
 		
@@ -79,7 +79,7 @@ END CPU_top;
 
 ARCHITECTURE beh OF CPU_top IS
 	
-	-- Reset debounce
+	-- Reset debounced and turned into active-low pulse
 	signal w_resetClean_n		: std_logic;
 	
 	-- CPU Peripheral bus
@@ -115,8 +115,17 @@ ARCHITECTURE beh OF CPU_top IS
 	
 	-- J12 Connectpr
 	signal w_io_J12				: std_logic_vector(36 downto 3);
+	
+	-- GPIO
+  signal w_dat0_i    			: std_logic_vector(2 downto 0);
+  signal w_dat0_o    			: std_logic_vector(2 downto 0);
+  signal w_n_dat0_oe 			: std_logic_vector(2 downto 0);
+  signal w_dat2_i    			: std_logic_vector(7 downto 0);
+  signal w_dat2_o    			: std_logic_vector(7 downto 0);
+  signal w_n_dat2_oe 			: std_logic_vector(7 downto 0);
+  signal w_GPIO_Out 				: std_logic_vector(7 downto 0);
 
-	-- Slow clock signals
+ -- Slow clock signals
 	signal w_slowPulse			: std_logic;
 	
 --	-- Signal Tap Logic Analyzer signals
@@ -168,6 +177,7 @@ BEGIN
 		w_timerOut									when (w_peripAddr(7 downto 2) = "000010") else		-- 0X08-0X0B = TIMER
 		io_J12(10 downto 3)						when (w_peripAddr = x"0C") else							-- 0X0C = Readback output latch
 		io_J12(36 downto 29)						when (w_peripAddr = x"0D") else							-- 0X0D = Readback output latch
+		w_GPIO_Out									when (w_peripAddr(7 downto 1) = "0000111") else		-- 0X0E-0x0F = GPIO
 		x"00";
 
 	-- -----------------------------------------------------------------------------------------------------------------
@@ -175,6 +185,7 @@ BEGIN
 	
 	-- ____________________________________________________________________________________
 	-- CPU Write Latches
+	-- Write when w_peripWr and address
 	WriteLatches : process (i_clock)
 	begin
 		if rising_edge(i_clock) then
@@ -204,7 +215,9 @@ BEGIN
 	);
 	
 	-- ____________________________________________________________________________________
-	-- Grant's VGA driver
+	-- Grant Searle's VGA driver from Multicomp
+	-- DGG removed the PS/2 keyboard, Composite output and CTS
+	-- Interface matches ACIA software interface address/control/status contents
 	o_vga_r <= w_videoR(1)&w_videoR(1)&w_videoR(0)&w_videoR(0)&w_videoR(0);					-- Map VGA pins 2:2:2 to 5:6:5
 	o_vga_g <= w_videoG(1)&w_videoG(1)&w_videoG(0)&w_videoG(0)&w_videoG(0)&w_videoG(0);
 	o_vga_b <= w_videoB(1)&w_videoB(1)&w_videoB(0)&w_videoB(0)&w_videoB(0);
@@ -212,7 +225,16 @@ BEGIN
 	W_VDUWr <= '1' when ((w_peripAddr(7 downto 1) = "0000011") and (w_peripWr = '1')) else '0';
 	W_VDURd <= '1' when ((w_peripAddr(7 downto 1) = "0000011") and (w_peripRd = '1')) else '0';
 	
+	-- EXTENDED_CHARSET=0, COLOUR_ATTS_ENABLED=0 - Uses 3 M9K blocks
+	-- EXTENDED_CHARSET=1, COLOUR_ATTS_ENABLED=0 - Uses 4 M9K blocks
+	-- EXTENDED_CHARSET=0, COLOUR_ATTS_ENABLED=1 - Uses 5 M9K blocks
+	-- EXTENDED_CHARSET=1, COLOUR_ATTS_ENABLED=1 - Uses 6 M9K blocks
 	vdu : entity work.ANSIDisplayVGA
+	GENERIC map (
+		EXTENDED_CHARSET		=> 0,
+		COLOUR_ATTS_ENABLED	=> 0,
+		SANS_SERIF_FONT		=> 1
+	)
 		port map (
 			clk		=> i_clock,
 			n_reset	=> w_resetClean_n,
@@ -235,6 +257,10 @@ BEGIN
 	
 	-- ____________________________________________________________________________________
 	-- ACIA UART serial interface
+	-- Grant Searle's UART driver from Multicomp
+	-- Modified by Neal Crook from Grant Searle's code to use rising clk
+	--		and to use baud rate enables rather than clocks
+	-- https://github.com/nealcrook/multicomp6809
 	w_UARTWr <= '1' when ((w_peripAddr(7 downto 1) = "0000010") and (w_peripWr = '1')) else '0';
 	W_UARTRd <= '1' when ((w_peripAddr(7 downto 1) = "0000010") and (w_peripRd = '1')) else '0';
 
@@ -253,7 +279,7 @@ BEGIN
 		);
 		
 	-- ____________________________________________________________________________________
-	-- Baud Rate Generator
+	-- Baud Rate Generator as an entity with passed baud rate
 	-- Legal BAUD_RATE values are 115200, 38400, 19200, 9600, 4800, 2400, 1200, 600, 300
 	BaudRateGen : entity work.BaudRate6850
 	GENERIC map (
@@ -280,6 +306,61 @@ BEGIN
 			i_dataIn				=> w_peripDataFromCPU,
 			o_dataOut			=> w_timerOut
 		);
+	
+	-- ____________________________________________________________________________________
+	-- GPIO Unit by Neal Crook - Built for 6809 Multicomp
+	-- https://github.com/nealcrook/multicomp6809/blob/master/multicomp/Components/GPIO/gpio.vhd
+	gpio : entity WORK.gpio
+	port MAP (
+        n_reset 	=> w_resetClean_n,
+        clk     	=> i_clock,
+        hold    	=> '0',
+        -- conditioned with chip select externally
+        n_wr    	=> not w_peripWr,
+        dataIn  	=> w_peripDataFromCPU,
+        dataOut 	=> w_GPIO_Out,
+        -- 0 for GPIOADR, 1 for GPIODAT
+        regAddr 	=> w_peripAddr(0),
+
+        -- GPIO
+        dat0_i    => w_dat0_i,
+        dat0_o    => w_dat0_o,
+        n_dat0_oe => w_n_dat0_oe,
+
+        dat2_i    => w_dat2_i,
+        dat2_o    => w_dat2_o,
+        n_dat2_oe => w_n_dat2_oe
+	);
+	-- io_J12(11) 
+	
+	
+	-- io_J12(11) to io_J12(21)
+	-- 
+   -- pin control.
+	-- There's probably an easier way of doing this??
+--    gpio_dat0_i <= gpio0;
+--    pad_ctl_gpio0: process(gpio_dat0_o, n_gpio_dat0_oe)
+--    begin
+--      for gpio_bit in 0 to 2 loop
+--        if n_gpio_dat0_oe(gpio_bit) = '0' then
+--          io_J12(11gpio_bit) <= gpio_dat0_o(gpio_bit);
+--        else
+--          gpio0(gpio_bit) <= 'Z';
+--        end if;
+--      end loop;
+--    end process;
+--
+--    gpio_dat2_i <= gpio2;
+--    pad_ctl_gpio2: process(gpio_dat2_o, n_gpio_dat2_oe)
+--    begin
+--      for gpio_bit in 0 to 7 loop
+--        if n_gpio_dat2_oe(gpio_bit) = '0' then
+--          gpio2(gpio_bit) <= gpio_dat2_o(gpio_bit);
+--        else
+--          gpio2(gpio_bit) <= 'Z';
+--        end if;
+--      end loop;
+--    end process;
 	
 	-- ____________________________________________________________________________________
 	-- Slow clock - used for debugging CPU
