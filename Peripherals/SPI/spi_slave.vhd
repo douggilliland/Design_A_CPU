112c8c1c94630acfd45c3c877e61e1a27cefe2ae
Doug Gilliland <doug@douglasgilliland.com>
2022-04-09 12:01:43 -0400

    Update spi_slave.vhd

--- Peripherals/SPI/spi_slave.vhd
+++ Peripherals/SPI/spi_slave.vhd
@@ -1,214 +1,70 @@
---------------------------------------------------------------------------------
--- PROJECT: SPI MASTER AND SLAVE FOR FPGA
---------------------------------------------------------------------------------
--- NAME:    SPI_SLAVE
--- AUTHORS: Jakub Cabal <jakubcabal@gmail.com>
--- LICENSE: LGPL-3.0, please read LICENSE file
--- WEBSITE: https://github.com/jakubcabal/spi-fpga
---------------------------------------------------------------------------------
--- COPYRIGHT NOTICE:
---------------------------------------------------------------------------------
--- SPI MASTER AND SLAVE FOR FPGA
--- Copyright (C) 2016 Jakub Cabal
---
--- This source file is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Lesser General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This source file is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU Lesser General Public License for more details.
---
--- You should have received a copy of the GNU Lesser General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
---------------------------------------------------------------------------------
+== SPI Interface ==
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+From https://github.com/jakubcabal
 
--- THE SPI SLAVE MODULE SUPPORT ONLY SPI MODE 0 (CPOL=0, CPHA=0)!!!
+<img src="https://raw.githubusercontent.com/douggilliland/MultiComp/master/MultiComp%20(VHDL%20Template)/Components/SPI/jakubcabal/SPI_VHDL_Docs.png></img>
 
-entity SPI_SLAVE is
-    Port (
-        CLK      : in  std_logic; -- system clock
-        RST      : in  std_logic; -- high active synchronous reset
-        -- SPI SLAVE INTERFACE
-        SCLK     : in  std_logic; -- SPI clock
-        CS_N     : in  std_logic; -- SPI chip select, active in low
-        MOSI     : in  std_logic; -- SPI serial data from master to slave
-        MISO     : out std_logic; -- SPI serial data from slave to master
-        -- USER INTERFACE
-        DIN      : in  std_logic_vector(7 downto 0); -- input data for SPI master
-        DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, input data are valid
-        READY    : out std_logic; -- when READY = 1, valid input data are accept
-        DOUT     : out std_logic_vector(7 downto 0); -- output data from SPI master
-        DOUT_VLD : out std_logic  -- when DOUT_VLD = 1, output data are valid
-    );
-end SPI_SLAVE;
+# SPI MASTER AND SLAVE FOR FPGA
 
-architecture RTL of SPI_SLAVE is
+The [https://github.com/douggilliland/Design_A_CPU/blob/main/Peripherals/SPI/spi_master.vhd SPI master] and [https://github.com/douggilliland/Design_A_CPU/blob/main/Peripherals/SPI/spi_slave.vhdSPI slave] are simple controllers for communication between FPGA and various peripherals via the SPI interface.
+* The SPI master and SPI slave have been implemented using VHDL 93 and are applicable to any FPGA.
+**The SPI master and SPI slave controllers support only SPI mode 0 (CPOL=0, CPHA=0)!**
+* The SPI master and SPI slave controllers were simulated and tested in hardware. 
 
-    signal spi_clk_reg        : std_logic;
-    signal spi_clk_redge_en   : std_logic;
-    signal spi_clk_fedge_en   : std_logic;
-    signal bit_cnt            : unsigned(2 downto 0);
-    signal bit_cnt_max        : std_logic;
-    signal last_bit_en        : std_logic;
-    signal load_data_en       : std_logic;
-    signal data_shreg         : std_logic_vector(7 downto 0);
-    signal slave_ready        : std_logic;
-    signal shreg_busy         : std_logic;
-    signal rx_data_vld        : std_logic;
+== SPI master ==
 
-begin
+=== Generics ===
 
-    -- -------------------------------------------------------------------------
-    --  SPI CLOCK REGISTER
-    -- -------------------------------------------------------------------------
+<pre>
+CLK_FREQ    : natural := 50e6; -- set system clock frequency in Hz
+SCLK_FREQ   : natural := 5e6;  -- set SPI clock frequency in Hz (condition: SCLK_FREQ <= CLK_FREQ/10)
+WORD_SIZE   : natural := 8;    -- size of transfer word in bits, must be power of two
+SLAVE_COUNT : natural := 1     -- count of SPI slaves
+</pre>
 
-    -- The SPI clock register is necessary for clock edge detection.
-    spi_clk_reg_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (RST = '1') then
-                spi_clk_reg <= '0';
-            else
-                spi_clk_reg <= SCLK;
-            end if;
-        end if;
-    end process;
+=== Ports ===
 
-    -- -------------------------------------------------------------------------
-    --  SPI CLOCK EDGES FLAGS
-    -- -------------------------------------------------------------------------
+<pre>
+CLK      : in  std_logic; -- system clock
+RST      : in  std_logic; -- high active synchronous reset
+-- SPI MASTER INTERFACE
+SCLK     : out std_logic; -- SPI clock
+CS_N     : out std_logic_vector(SLAVE_COUNT-1 downto 0); -- SPI chip select, active in low
+MOSI     : out std_logic; -- SPI serial data from master to slave
+MISO     : in  std_logic; -- SPI serial data from slave to master
+-- INPUT USER INTERFACE
+DIN      : in  std_logic_vector(WORD_SIZE-1 downto 0); -- data for transmission to SPI slave
+DIN_ADDR : in  std_logic_vector(natural(ceil(log2(real(SLAVE_COUNT))))-1 downto 0); -- SPI slave address
+DIN_LAST : in  std_logic; -- when DIN_LAST = 1, last data word, after transmit will be asserted CS_N
+DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, data for transmission are valid
+DIN_RDY  : out std_logic; -- when DIN_RDY = 1, SPI master is ready to accept valid data for transmission
+-- OUTPUT USER INTERFACE
+DOUT     : out std_logic_vector(WORD_SIZE-1 downto 0); -- received data from SPI slave
+DOUT_VLD : out std_logic  -- when DOUT_VLD = 1, received data are valid
+</pre>
 
-    -- Falling edge is detect when SCLK=0 and spi_clk_reg=1.
-    spi_clk_fedge_en <= not SCLK and spi_clk_reg;
-    -- Rising edge is detect when SCLK=1 and spi_clk_reg=0.
-    spi_clk_redge_en <= SCLK and not spi_clk_reg;
+== SPI slave ==
 
-    -- -------------------------------------------------------------------------
-    --  RECEIVED BITS COUNTER
-    -- -------------------------------------------------------------------------
+===  Generics ===
 
-    -- The counter counts received bits from the master. Counter is enabled when
-    -- falling edge of SPI clock is detected and not asserted CS_N.
-    bit_cnt_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (RST = '1') then
-                bit_cnt <= (others => '0');
-            elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
-                if (bit_cnt_max = '1') then
-                    bit_cnt <= (others => '0');
-                else
-                    bit_cnt <= bit_cnt + 1;
-                end if;
-            end if;
-        end if;
-    end process;
+<pre>
+WORD_SIZE : natural := 8; -- size of transfer word in bits, must be power of two
+</pre>
 
-    -- The flag of maximal value of the bit counter.
-    bit_cnt_max <= '1' when (bit_cnt = "111") else '0';
+=== Ports ===
 
-    -- -------------------------------------------------------------------------
-    --  LAST BIT FLAG REGISTER
-    -- -------------------------------------------------------------------------
-
-    -- The flag of last bit of received byte is only registered the flag of
-    -- maximal value of the bit counter.
-    last_bit_en_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (RST = '1') then
-                last_bit_en <= '0';
-            else
-                last_bit_en <= bit_cnt_max;
-            end if;
-        end if;
-    end process;
-
-    -- -------------------------------------------------------------------------
-    --  RECEIVED DATA VALID FLAG
-    -- -------------------------------------------------------------------------
-
-    -- Received data from master are valid when falling edge of SPI clock is
-    -- detected and the last bit of received byte is detected.
-    rx_data_vld <= spi_clk_fedge_en and last_bit_en;
-
-    -- -------------------------------------------------------------------------
-    --  SHIFT REGISTER BUSY FLAG REGISTER
-    -- -------------------------------------------------------------------------
-
-    -- Data shift register is busy until it sends all input data to SPI master.
-    shreg_busy_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (RST = '1') then
-                shreg_busy <= '0';
-            else
-                if (DIN_VLD = '1' and (CS_N = '1' or rx_data_vld = '1')) then
-                    shreg_busy <= '1';
-                elsif (rx_data_vld = '1') then
-                    shreg_busy <= '0';
-                else
-                    shreg_busy <= shreg_busy;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    -- The SPI slave is ready for accept new input data when CS_N is assert and
-    -- shift register not busy or when received data are valid.
-    slave_ready <= (CS_N and not shreg_busy) or rx_data_vld;
-    
-    -- The new input data is loaded into the shift register when the SPI slave
-    -- is ready and input data are valid.
-    load_data_en <= slave_ready and DIN_VLD;
-
-    -- -------------------------------------------------------------------------
-    --  DATA SHIFT REGISTER
-    -- -------------------------------------------------------------------------
-
-    -- The shift register holds data for sending to master, capture and store
-    -- incoming data from master.
-    data_shreg_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (load_data_en = '1') then
-                data_shreg <= DIN;
-            elsif (spi_clk_redge_en = '1' and CS_N = '0') then
-                data_shreg <= data_shreg(6 downto 0) & MOSI;
-            end if;
-        end if;
-    end process;
-
-    -- -------------------------------------------------------------------------
-    --  MISO REGISTER
-    -- -------------------------------------------------------------------------
-
-    -- The output MISO register ensures that the bits are transmit to the master
-    -- when is not assert CS_N and falling edge of SPI clock is detected.
-    miso_p : process (CLK)
-    begin
-        if (rising_edge(CLK)) then
-            if (load_data_en = '1') then
-                MISO <= DIN(7);
-            elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
-                MISO <= data_shreg(7);
-            end if;
-        end if;
-    end process;
-
-    -- -------------------------------------------------------------------------
-    --  ASSIGNING OUTPUT SIGNALS
-    -- -------------------------------------------------------------------------
-    
-    READY    <= slave_ready;
-    DOUT     <= data_shreg;
-    DOUT_VLD <= rx_data_vld;
-
-end RTL;
+<pre>
+CLK      : in  std_logic; -- system clock
+RST      : in  std_logic; -- high active synchronous reset
+-- SPI SLAVE INTERFACE
+SCLK     : in  std_logic; -- SPI clock
+CS_N     : in  std_logic; -- SPI chip select, active in low
+MOSI     : in  std_logic; -- SPI serial data from master to slave
+MISO     : out std_logic; -- SPI serial data from slave to master
+-- USER INTERFACE
+DIN      : in  std_logic_vector(WORD_SIZE-1 downto 0); -- data for transmission to SPI master
+DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, data for transmission are valid
+DIN_RDY  : out std_logic; -- when DIN_RDY = 1, SPI slave is ready to accept valid data for transmission
+DOUT     : out std_logic_vector(WORD_SIZE-1 downto 0); -- received data from SPI master
+DOUT_VLD : out std_logic  -- when DOUT_VLD = 1, received data are valid
+</pre>
